4b0d4a0e84a2fc9f10be4d51efddf17b60241dd7
DavidFloyd <david414tecno@gmail.com>
2017-05-25 16:12:12 +0100

    basic page

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,4 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
-import { RootComponent } from './root/root.component';
-import { myRouterProviders } from './root/root.routes';
+import { AppComponent } from './app.component';
 
-bootstrap(RootComponent, [myRouterProviders]);
-
+bootstrap(AppComponent);
